7b1b4e3966020b70aedd60bcdc39ac33a40b3ffa
amandine1799 <gaigniere.amandine@live.fr>
2019-08-13 11:19:08 +0200

    amandine1799

---
+++ src/Form/ReviewType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('review')
+            ->add('date')
+            ->add('rating')
+            ->add('media')
+            ->add('users')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
