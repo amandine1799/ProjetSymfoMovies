e12ecc268d7464296d4a2b688be2d209b2889a66
Amandine <gaigniere.amandine@live.fr>
2019-09-07 18:36:48 +0200

    front

---
+++ src/Migrations/Version20190907150117.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190907150117 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE users_roles (users_id INT NOT NULL, roles_id INT NOT NULL, INDEX IDX_51498A8E67B3B43D (users_id), INDEX IDX_51498A8E38C751C4 (roles_id), PRIMARY KEY(users_id, roles_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE roles (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8E67B3B43D FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8E38C751C4 FOREIGN KEY (roles_id) REFERENCES roles (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE actors CHANGE died died VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE media_users CHANGE media_id media_id INT DEFAULT NULL, CHANGE users_id users_id INT DEFAULT NULL, CHANGE rating rating INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE media CHANGE trailer trailer VARCHAR(500) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users_roles DROP FOREIGN KEY FK_51498A8E38C751C4');
+        $this->addSql('DROP TABLE users_roles');
+        $this->addSql('DROP TABLE roles');
+        $this->addSql('ALTER TABLE actors CHANGE died died VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE media CHANGE trailer trailer VARCHAR(500) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE media_users CHANGE media_id media_id INT DEFAULT NULL, CHANGE users_id users_id INT DEFAULT NULL, CHANGE rating rating INT DEFAULT NULL');
+    }
+}
